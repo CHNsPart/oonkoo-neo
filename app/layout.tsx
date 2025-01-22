359151619ca0be8f1fbb0e4bdcf56b5fe8ff0576
chnspart <imchn24@gmail.com>
2025-01-22 00:59:59 -0500

    chore: Vercel & Google Script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import { BlobCursor } from "@/components/cursor/blob-cursor";
 import Footer from "@/components/pages/footer";
 import LoaderWrapper from "@/components/wrapper/loader-wrapper";
 import { KindeProvider } from "@/components/providers/auth-provider";
+import Script from "next/script";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +33,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights/>
+      <Analytics />
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-W4HD10V1HG"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-W4HD10V1HG');
+        `}
+      </Script>
+
+      {/* Smartlook */}
+      <Script id="smartlook" strategy="afterInteractive">
+        {`
+          window.smartlook||(function(d) {
+            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+            c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+          })(document);
+          smartlook('init', '1495fd30a37930fbcee54486b7a25be75352b540', { region: 'eu' });
+        `}
+      </Script>
       <KindeProvider>
         <body 
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black min-h-screen relative`}
