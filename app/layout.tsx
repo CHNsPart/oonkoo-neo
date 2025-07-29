bf582214433d8705246f5175e1f12a6eb6eb166d
chnspart <imchn24@gmail.com>
2025-07-29 13:43:47 -0400

    Meta Pixel Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,6 +119,20 @@ export default function RootLayout({
           gtag('config', 'G-W4HD10V1HG');
         `}
       </Script>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2301839860213124');
+          fbq('track', 'PageView');
+        `}
+      </Script>
       <KindeProvider>
         <body 
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black min-h-screen relative`}
